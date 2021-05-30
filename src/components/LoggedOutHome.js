2ed00d3c758e3085da998d3c4999d3e6dddd7b4f
Lee Yat Bun <benlee1300@gmail.com>
2021-05-30 19:53:15 +0800

    Visual changes for release - Changed favico - Updated page title - Updated homepage

--- src/components/LoggedOutHome.js
+++ src/components/LoggedOutHome.js
@@ -1,22 +1,29 @@
-import { Container } from "react-bootstrap";
-import { CustomPlaceholder } from "react-placeholder-image";
+import { Container, Image } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+import IntroPic from "../img/intro.png";
+import TeamsPic from "../img/teams.png";
+import TasksPic from "../img/tasks.png";
 
 export default function LoggedOutHome() {
     return (
         <>
             <div className="px-4 my-5 text-center border-bottom">
-                <h1 className="display-4 fw-bold">Improve your workflow!</h1>
+                <h1 className="display-4 fw-bold">Be ahead of the game</h1>
 
                 <div className="col-lg-6 mx-auto">
                     <p className="lead mb-4">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eu dui semper,
-                        volutpat velit non, sagittis ex. Vivamus elementum dui diam, ac pharetra
-                        augue sodales id. Pellentesque id arcu diam. Vestibulum fermentum justo
-                        sapien, quis viverra dolor feugiat ac. Ut vitae lacus mi.
+                        Customise groups, delegate tasks and monitor deadlines. With Wurkflow, you
+                        can now manage your team and the work to be done simulatenously! Never worry
+                        about troublesome deadlines again.
                     </p>
 
                     <div className="d-grid gap-2 d-sm-flex justify-content-sm-center mb-5">
-                        <button className="btn btn-danger btn-lg px-4 me-sm-3">Learn More</button>
+                        <Link to="/signup">
+                            <button className="btn btn-danger btn-lg px-4 me-sm-3">
+                                Create Account
+                            </button>
+                        </Link>
                         <button className="btn btn-outline-danger btn-lg px-4">
                             See It In Action
                         </button>
@@ -25,42 +32,36 @@ export default function LoggedOutHome() {
 
                 <div className="overflow-hidden" style={{ maxHeight: "30vh" }}>
                     <Container className="px-5">
-                        <CustomPlaceholder
-                            className="border rounded-3 shadow-lg mb-4"
-                            width={700}
-                            height={400}
-                            text="Screenshot here"
-                        />
+                        <Image src={IntroPic} width={700} height={270} />
                     </Container>
                 </div>
             </div>
 
             <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
                 <div className="col-10 col-sm-8 col-lg-6">
-                    <CustomPlaceholder
-                        className="d-block mx-lg-auto"
+                    <Image
+                        src={TeamsPic}
                         width={500}
-                        height={300}
-                        text="Screenshot"
+                        height={270}
+                        className="border shadow-lg rounded"
                     />
                 </div>
 
                 <div className="col-lg-6">
-                    <h1 className="display-5 fw-bold lh-1 mb-3">Feature 1</h1>
+                    <h1 className="display-5 fw-bold lh-1 mb-3">Groups</h1>
                     <p className="lead">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eu dui semper,
-                        volutpat velit non, sagittis ex. Vivamus elementum dui diam, ac pharetra
-                        augue sodales id. Pellentesque id arcu diam. Vestibulum fermentum justo
-                        sapien, quis viverra dolor feugiat ac. Ut vitae lacus mi.
+                        Begin your journey by joining a group - or creating them yourself. With
+                        groups, delegation of work can now be done in a systematic and organised
+                        way.
                     </p>
 
                     <div className="d-grid gap-2 d-md-flex justify-content-md-start">
-                        <button className="btn btn-danger btn-lg px-4 me-md-2">Learn More</button>
+                        <button className="btn btn-danger btn-lg px-4 me-md-2">See More</button>
                     </div>
                 </div>
             </div>
 
-            <div className="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border shadow-lg">
+            {/* <div className="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border shadow-lg">
                 <div className="p-3">
                     <h1 className="display-4 fw-bold lh-1 mb-4">See what others say</h1>
                     <p className="lead mb-4">
@@ -70,25 +71,23 @@ export default function LoggedOutHome() {
                         sapien, quis viverra dolor feugiat ac. Ut vitae lacus mi.
                     </p>
                 </div>
-            </div>
+            </div> */}
 
             <div className="row flex-lg-row align-items-center g-5 py-5">
                 <div className="col-10 col-sm-8 col-lg-6">
-                    <CustomPlaceholder
-                        className="d-block mx-lg-auto"
+                    <Image
+                        src={TasksPic}
                         width={500}
-                        height={300}
-                        text="Screenshot"
+                        height={270}
+                        className="border shadow-lg rounded"
                     />
                 </div>
 
                 <div className="col-lg-6">
-                    <h1 className="display-5 fw-bold lh-1 mb-3">Feature 2</h1>
+                    <h1 className="display-5 fw-bold lh-1 mb-3">Tasks</h1>
                     <p className="lead">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eu dui semper,
-                        volutpat velit non, sagittis ex. Vivamus elementum dui diam, ac pharetra
-                        augue sodales id. Pellentesque id arcu diam. Vestibulum fermentum justo
-                        sapien, quis viverra dolor feugiat ac. Ut vitae lacus mi.
+                        Create tasks for your team and customise deadlines as you see fit! Let
+                        Wurkflow notify the respective members as you sit back and monitor the task.
                     </p>
 
                     <div className="d-grid gap-2 d-md-flex justify-content-md-start">
