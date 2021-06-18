11d57ef5e11549c04f7f175b59c59e2eb7653b48
Lee Yat Bun <benlee1300@gmail.com>
2021-06-18 14:36:29 +0800

    Further refined new workflow page - Locked template settings after confirmation to prevent changing teams - Set limit for due dates

--- src/components/NewWorkflow.js
+++ src/components/NewWorkflow.js
@@ -1,10 +1,10 @@
 import { useRef, useState, useEffect } from "react";
 import { store } from "../firebase";
 import { useStore } from "../contexts/StoreContext";
-import { add } from "date-fns";
+import { differenceInCalendarDays, add, sub } from "date-fns";
 import { DateLocalizer } from "react-widgets/IntlLocalizer";
 
-import { Container, Form, Button, ButtonGroup, Badge, Card, Spinner } from "react-bootstrap";
+import { Container, Alert, Form, Button, ButtonGroup, Badge, Card, Spinner } from "react-bootstrap";
 
 import PageHeader from "./PageHeader";
 import DropdownList from "react-widgets/DropdownList";
@@ -16,6 +16,8 @@ import NumberPicker from "react-widgets/NumberPicker";
 export default function NewWorkflow() {
     const { teams } = useStore();
 
+    const [error, setError] = useState("");
+    const [continued, setContinued] = useState(false);
     const wfNameRef = useRef();
     const wfDescRef = useRef();
     const [wfDate, setWfDate] = useState(new Date());
@@ -46,7 +48,7 @@ export default function NewWorkflow() {
             });
     }
 
-    const blankTask = { name: "", desc: "", users: [], dueDate: wfDate };
+    const blankTask = { name: "", desc: "", users: [], daysBefore: 0, dueDate: wfDate };
     const [taskData, setTaskData] = useState([{ ...blankTask }]);
 
     function handleAdd(e) {
@@ -70,7 +72,10 @@ export default function NewWorkflow() {
     }
 
     function handleDateChange(idx, date) {
+        const dateDiff = differenceInCalendarDays(wfDate, date);
+
         const updatedTaskData = [...taskData];
+        updatedTaskData[idx]["daysBefore"] = dateDiff;
         updatedTaskData[idx]["dueDate"] = date;
         setTaskData(updatedTaskData);
     }
@@ -79,6 +84,16 @@ export default function NewWorkflow() {
         const updatedTaskData = [...taskData];
         updatedTaskData[idx]["users"] = val;
         setTaskData(updatedTaskData);
+    }
+
+    function handleContinue() {
+        setError("");
+
+        if (!wfNameRef.current.value || !wfDescRef.current.value || !wfTeam) {
+            setError("Please fill in the template settings");
+        } else {
+            setContinued(true);
+        }
     }
 
     const [loading, setLoading] = useState(false);
@@ -99,10 +114,131 @@ export default function NewWorkflow() {
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [wfTeam]);
 
+    const renderWfTeams = () => {
+        return (
+            <>
+                <h2 className="mt-5">
+                    Template Tasks{" "}
+                    <Badge variant="secondary">
+                        {taskData.length === 1 ? "1 Task" : taskData.length + " Tasks"}
+                    </Badge>
+                </h2>
+
+                <ButtonGroup className="mt-4 w-100">
+                    <Button variant="outline-primary" onClick={handleAdd} className="w-50">
+                        Add Task
+                    </Button>
+                    <Button
+                        variant="outline-danger"
+                        onClick={handleRemove}
+                        className="w-50"
+                        disabled={taskData.length <= 1}
+                    >
+                        Remove Task
+                    </Button>
+                </ButtonGroup>
+
+                {taskData.map((_, idx) => {
+                    const nameId = `name-${idx}`;
+                    const descId = `desc-${idx}`;
+
+                    return (
+                        <Card key={idx} className="p-4 mt-4">
+                            <Card.Title>Task {idx + 1}</Card.Title>
+                            <Form.Group>
+                                <Form.Label>Task Name</Form.Label>
+                                <Form.Control
+                                    type="text"
+                                    name={nameId}
+                                    data-idx={idx}
+                                    id="name"
+                                    value={taskData[idx].name}
+                                    onChange={handleTaskDataChange}
+                                    required
+                                />
+                            </Form.Group>
+
+                            <Form.Group>
+                                <Form.Label>Task Description</Form.Label>
+                                <Form.Control
+                                    as="textarea"
+                                    row={3}
+                                    name={descId}
+                                    data-idx={idx}
+                                    id="desc"
+                                    value={taskData[idx].desc}
+                                    onChange={handleTaskDataChange}
+                                    required
+                                />
+                            </Form.Group>
+
+                            <Form.Row>
+                                <Container className="w-50">
+                                    <Form.Group>
+                                        <Form.Label>Task Due Date</Form.Label>
+                                        <br />
+                                        <DatePicker
+                                            value={taskData[idx].dueDate}
+                                            min={
+                                                idx === 0
+                                                    ? new Date(2020, 1, 1, 0, 0)
+                                                    : add(taskData[idx - 1].dueDate, { days: 1 })
+                                            }
+                                            max={sub(wfDate, { days: 1 })}
+                                            valueDisplayFormat={{
+                                                dateStyle: "medium",
+                                            }}
+                                            onChange={(val) => {
+                                                handleDateChange(idx, val);
+                                            }}
+                                        />
+                                    </Form.Group>
+                                </Container>
+                                <Container className="w-50">
+                                    <Form.Group>
+                                        <Form.Label>Days Due Before</Form.Label>
+                                        <NumberPicker
+                                            value={taskData[idx].daysBefore}
+                                            min={0}
+                                            disabled
+                                        />
+                                    </Form.Group>
+                                </Container>
+                            </Form.Row>
+
+                            <Form.Group className="mt-4">
+                                <Form.Label>Users Involved</Form.Label>
+                                <Multiselect
+                                    defaultValue={[]}
+                                    data={teamUsers.map((user) => user.name)}
+                                    onChange={(sel) => handleUsersChange(idx, sel)}
+                                />
+                            </Form.Group>
+                        </Card>
+                    );
+                })}
+
+                <Button type="submit" className="w-100 mt-5" disabled={loading}>
+                    Create
+                    {loading && (
+                        <Spinner
+                            as="span"
+                            animation="border"
+                            size="sm"
+                            role="status"
+                            className="mx-2"
+                        />
+                    )}
+                </Button>
+            </>
+        );
+    };
+
     return (
         <>
             <PageHeader />
             <Container className="pt-5 mt-5">
+                {error && <Alert variant="danger">{error}</Alert>}
                 <Container className="d-flex flex-column">
                     <Container className="col-sm-12 mx-auto bg-light p-5 rounded">
                         <h1>New Workflow Template</h1>
@@ -121,12 +257,21 @@ export default function NewWorkflow() {
 
                                 <Form.Group className="mt-4">
                                     <Form.Label>Workflow Name</Form.Label>
-                                    <Form.Control type="text" ref={wfNameRef} />
+                                    <Form.Control
+                                        type="text"
+                                        ref={wfNameRef}
+                                        disabled={continued}
+                                    />
                                 </Form.Group>
 
                                 <Form.Group className="mt-4">
                                     <Form.Label>Workflow Description</Form.Label>
-                                    <Form.Control as="textarea" rows={3} ref={wfDescRef} />
+                                    <Form.Control
+                                        as="textarea"
+                                        rows={3}
+                                        ref={wfDescRef}
+                                        disabled={continued}
+                                    />
                                 </Form.Group>
 
                                 <Form.Group className="mt-4">
@@ -136,6 +281,7 @@ export default function NewWorkflow() {
                                         data={teams}
                                         textField="name"
                                         onChange={(val) => setWfTeam(val)}
+                                        disabled={continued}
                                     />
                                 </Form.Group>
 
@@ -147,127 +293,20 @@ export default function NewWorkflow() {
                                         onSelect={(date) => setWfDate(date)}
                                         valueEditFormat={{ dateStyle: "short" }}
                                         valueDisplayFormat={{ dateStyle: "medium" }}
-                                        format="mmm YYY"
+                                        disabled={continued}
                                     />
                                 </Form.Group>
 
-                                <h2 className="mt-5">
-                                    Template Tasks{" "}
-                                    <Badge variant="secondary">
-                                        {taskData.length === 1
-                                            ? "1 Task"
-                                            : taskData.length + " Tasks"}
-                                    </Badge>
-                                </h2>
-
-                                <ButtonGroup className="mt-4 w-100">
-                                    <Button
-                                        variant="outline-primary"
-                                        onClick={handleAdd}
-                                        className="w-50"
-                                    >
-                                        Add Task
-                                    </Button>
-                                    <Button
-                                        variant="outline-danger"
-                                        onClick={handleRemove}
-                                        className="w-50"
-                                        disabled={taskData.length <= 1}
-                                    >
-                                        Remove Task
-                                    </Button>
-                                </ButtonGroup>
-
-                                {taskData.map((val, idx) => {
-                                    const nameId = `name-${idx}`;
-                                    const descId = `desc-${idx}`;
-
-                                    return (
-                                        <Card key={idx} className="p-4 mt-4">
-                                            <Card.Title>Task {idx + 1}</Card.Title>
-                                            <Form.Group>
-                                                <Form.Label>Task Name</Form.Label>
-                                                <Form.Control
-                                                    type="text"
-                                                    name={nameId}
-                                                    data-idx={idx}
-                                                    id="name"
-                                                    value={taskData[idx].name}
-                                                    onChange={handleTaskDataChange}
-                                                    required
-                                                />
-                                            </Form.Group>
-
-                                            <Form.Group>
-                                                <Form.Label>Task Description</Form.Label>
-                                                <Form.Control
-                                                    as="textarea"
-                                                    row={3}
-                                                    name={descId}
-                                                    data-idx={idx}
-                                                    id="desc"
-                                                    value={taskData[idx].desc}
-                                                    onChange={handleTaskDataChange}
-                                                    required
-                                                />
-                                            </Form.Group>
-
-                                            <Form.Row>
-                                                <Container className="w-50">
-                                                    <Form.Group>
-                                                        <Form.Label>Days Due Before</Form.Label>
-                                                        <NumberPicker
-                                                            defaultValue={0}
-                                                            min={0}
-                                                            onChange={(val) => {
-                                                                const newDate = add(wfDate, {
-                                                                    days: -val,
-                                                                });
-
-                                                                handleDateChange(idx, newDate);
-                                                            }}
-                                                        />
-                                                    </Form.Group>
-                                                </Container>
-                                                <Container className="w-50">
-                                                    <Form.Group>
-                                                        <Form.Label>Task Due Date</Form.Label>
-                                                        <br />
-                                                        <DatePicker
-                                                            value={taskData[idx].dueDate}
-                                                            valueDisplayFormat={{
-                                                                dateStyle: "medium",
-                                                            }}
-                                                            disabled
-                                                        />
-                                                    </Form.Group>
-                                                </Container>
-                                            </Form.Row>
-
-                                            <Form.Group className="mt-4">
-                                                <Form.Label>Users Involved</Form.Label>
-                                                <Multiselect
-                                                    defaultValue={[]}
-                                                    data={teamUsers.map((user) => user.name)}
-                                                    onChange={(sel) => handleUsersChange(idx, sel)}
-                                                />
-                                            </Form.Group>
-                                        </Card>
-                                    );
-                                })}
-
-                                <Button type="submit" className="w-100 mt-5" disabled={loading}>
-                                    Create
-                                    {loading && (
-                                        <Spinner
-                                            as="span"
-                                            animation="border"
-                                            size="sm"
-                                            role="status"
-                                            className="mx-2"
-                                        />
-                                    )}
+                                <Button
+                                    variant="primary"
+                                    onClick={handleContinue}
+                                    className="w-100 mt-4"
+                                    disabled={continued}
+                                >
+                                    Continue
                                 </Button>
+
+                                {continued && renderWfTeams()}
                             </Form>
                         </Localization>
                     </Container>
