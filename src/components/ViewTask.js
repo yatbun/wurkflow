2269a9bef88aa6d00121aeb4ab563fce7ff00d27
reignnz <64293095+reignnz@users.noreply.github.com>
2021-07-20 01:57:37 +0800

    Commenting - Added a feature for the user to comment on the task assigned - Added a feature for the user to delete the comments that they posted

--- src/components/ViewTask.js
+++ src/components/ViewTask.js
@@ -1,17 +1,58 @@
-import { useState, useEffect } from "react";
-import { Container, Row, Col, Badge, ListGroup } from "react-bootstrap";
+import { useState, useEffect, useRef } from "react";
+import {
+    Container,
+    Row,
+    Col,
+    Badge,
+    ListGroup,
+    Form,
+    Button,
+    InputGroup,
+    Modal,
+} from "react-bootstrap";
 import { useParams, Link, useHistory } from "react-router-dom";
 import { store } from "../firebase";
+import { useAuth } from "../contexts/AuthContext";
+
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import { FaTrash, FaExclamationTriangle } from "react-icons/fa";
 
 import PageHeader from "./PageHeader";
+
+dayjs.extend(relativeTime);
 
 export default function ViewTask() {
     const { id } = useParams();
     const history = useHistory();
+    const { currentUser } = useAuth();
 
     const [task, setTask] = useState(null);
     const [taskUsers, setTaskUsers] = useState([]);
     const [taskCreator, setTaskCreator] = useState(null);
+    const [comments, setComments] = useState([]);
+    const [cid, setCid] = useState();
+    const [action, setAction] = useState();
+    const [showModal, setShowModal] = useState(false);
+
+    const commentRef = useRef();
+
+    function modalAction() {
+        if (action === "remove") {
+            deleteComment(cid);
+        }
+        closeModal();
+    }
+
+    function openModal(action, cid) {
+        setShowModal(true);
+        setCid(cid);
+        setAction(action);
+    }
+
+    function closeModal() {
+        setShowModal(false);
+    }
 
     async function getTask() {
         store
@@ -55,10 +96,93 @@ export default function ViewTask() {
         });
     }
 
+    async function getComments() {
+        const comments = [];
+        const promise = [];
+        promise.push(
+            store
+                .collection("comments")
+                .where("taskId", "==", store.collection("tasks").doc(id))
+                .get()
+                .then((snapshot) => {
+                    snapshot.forEach((doc) => {
+                        const newComment = doc.data();
+                        newComment.dateCreated = newComment.createdAt.toDate();
+                        newComment.id = doc.id;
+                        comments.push(newComment);
+                    });
+                })
+        );
+        Promise.all(promise).then(() => {
+            comments.sort((a, b) => {
+                const na = a.dateCreated;
+                const nb = b.dateCreated;
+
+                if (na < nb) {
+                    return 1;
+                }
+                if (na > nb) {
+                    return -1;
+                }
+                return 0;
+            });
+            setComments(comments);
+        });
+    }
+
+    async function handleComment(e) {
+        e.preventDefault();
+
+        if (commentRef.current.value === "") {
+        }
+
+        const newComment = {
+            taskId: store.collection("tasks").doc(id),
+            userId: store.collection("users").doc(currentUser.uid),
+            name: currentUser.displayName,
+            body: commentRef.current.value,
+            createdAt: new Date(),
+        };
+
+        store
+            .collection("comments")
+            .add(newComment)
+            .then(() => {
+                getComments();
+            });
+    }
+
+    async function deleteComment(cid) {
+        store
+            .collection("comments")
+            .doc(cid)
+            .delete()
+            .then(() => {
+                getComments();
+            });
+    }
+
     const renderTasks = () => {
         if (task) {
             return (
                 <>
+                    <Modal show={showModal} onHide={closeModal}>
+                        <Modal.Header>
+                            <Modal.Title>
+                                <FaExclamationTriangle />
+                            </Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>Are you sure?</Modal.Body>
+                        <Modal.Footer>
+                            <Button variant="secondary" onClick={closeModal}>
+                                Cancel
+                            </Button>
+                            <Button variant="warning" onClick={modalAction}>
+                                I AM SURE
+                            </Button>
+                        </Modal.Footer>
+                    </Modal>
+
                     <Container className="col-sm-12 mx-auto bg-light p-5 rounded">
                         <Link
                             to={`/edit-task/${id}`}
@@ -85,7 +209,102 @@ export default function ViewTask() {
                     <Container className="my-4">
                         <Row>
                             <Col>
-                                <h5> Checklist </h5>
+                                <h5> Comments </h5>
+                                <p className="my-2">
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        width="20"
+                                        height="20"
+                                        fill="currentColor"
+                                        class="bi bi-person-circle"
+                                        viewBox="0 0 16 16"
+                                        className="mx-1"
+                                    >
+                                        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+                                        <path
+                                            fill-rule="evenodd"
+                                            d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"
+                                        />
+                                    </svg>{" "}
+                                    {currentUser.displayName}
+                                </p>
+                                <Form className="my-0" onSubmit={handleComment}>
+                                    <InputGroup>
+                                        <Form.Control
+                                            ref={commentRef}
+                                            type="text"
+                                            placeholder="Enter your Comment here..."
+                                        ></Form.Control>
+                                        <Button
+                                            type="submit"
+                                            variant="light"
+                                            size="sm"
+                                            className=""
+                                        >
+                                            {" "}
+                                            Comment{" "}
+                                        </Button>
+                                    </InputGroup>
+                                </Form>
+
+                                <ListGroup variant="flush" className="my-5">
+                                    {comments &&
+                                        comments.map((comment) => (
+                                            <ListGroup.Item>
+                                                <Row>
+                                                    <Col>
+                                                        <svg
+                                                            xmlns="http://www.w3.org/2000/svg"
+                                                            width="20"
+                                                            height="20"
+                                                            fill="currentColor"
+                                                            class="bi bi-person-circle"
+                                                            viewBox="0 0 16 16"
+                                                            className="mx-1"
+                                                        >
+                                                            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+                                                            <path
+                                                                fill-rule="evenodd"
+                                                                d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"
+                                                            />
+                                                        </svg>
+                                                    </Col>
+                                                    <Col xs={9}>
+                                                        <p className="font-weight-bold">
+                                                            {" "}
+                                                            {comment.name}
+                                                        </p>
+                                                        {comment.body}{" "}
+                                                        <p
+                                                            className="text-muted"
+                                                            style={{ fontSize: 14 }}
+                                                        >
+                                                            {" "}
+                                                            {dayjs(comment.createdAt.toDate()).from(
+                                                                dayjs(new Date())
+                                                            )}
+                                                        </p>{" "}
+                                                    </Col>
+
+                                                    <Col>
+                                                        {currentUser.uid === comment.userId.id ? (
+                                                            <Button
+                                                                variant="danger"
+                                                                size="sm"
+                                                                onClick={() =>
+                                                                    openModal("remove", comment.id)
+                                                                }
+                                                            >
+                                                                <FaTrash />
+                                                            </Button>
+                                                        ) : (
+                                                            <div></div>
+                                                        )}
+                                                    </Col>
+                                                </Row>
+                                            </ListGroup.Item>
+                                        ))}
+                                </ListGroup>
                             </Col>
 
                             <Col>
@@ -116,6 +335,7 @@ export default function ViewTask() {
 
     useEffect(() => {
         getTask();
+        getComments();
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
