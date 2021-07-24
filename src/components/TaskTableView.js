fc7f1bffa50e96f67a63880348bd5837b045f71c
Lee Yat Bun <benlee1300@gmail.com>
2021-07-24 22:35:41 +0800

    Fixed table styling

--- src/components/TaskTableView.js
+++ src/components/TaskTableView.js
@@ -19,7 +19,7 @@ function TaskTableView({ tasks }) {
                     <th>Description</th>
                     <th>Date Due</th>
                     <th>Team</th>
-                    <th>Actions</th>
+                    <th style={{ minWidth: "150px" }}>Actions</th>
                 </tr>
             </thead>
 
